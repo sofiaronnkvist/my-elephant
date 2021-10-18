f75224b5e11075490209cc8170722f4596ede016
sofiaronnkvist <sofia@ronnkvist.com>
2021-10-18 11:39:43 +0200

    Added navbar and created more pages

--- PHP/header.php
+++ PHP/header.php
@@ -6,8 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Elephant</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/Styles/style-header.css">
 </head>
 
 <body>
-    <header></header>
+    <nav class="nav">
+        <input type="checkbox" id="nav-checkbox" class="nav-checkbox">
+        <label for="nav-checkbox" class="nav-toggle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="menu icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <line x1="4" y1="6" x2="20" y2="6"></line>
+                <line x1="4" y1="12" x2="20" y2="12"></line>
+                <line x1="4" y1="18" x2="20" y2="18"></line>
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="close icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+        </label>
+        <ul class="nav-menu">
+            <li class="nav-item"><a class="nav-link" href="#"></a>Hem</li>
+            <li class="nav-item"><a class="nav-link" href="#"></a>Frukt och grönt</li>
+            <li class="nav-item"><a class="nav-link" href="#"></a>Självplock</li>
+            <li class="nav-item"><a class="nav-link" href="#"></a>Om säsonger</li>
+        </ul>
+    </nav>
